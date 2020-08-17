dbcf39150500f65d4927522926e029efe4f8ba2b
Emmanouilidis Konstantinos <emmanouilidis.kons@gmail.com>
2020-08-17 14:39:58 +0300

    Delete rook_move.py

--- Code/Exercises 1st Set/rook_move.py
+++
@@ -1,8 +0,0 @@
-r1 = int(input("Insert the row of the first cell: "))
-c1 = int(input("Insert the column of the first cell: "))
-r2 = int(input("Insert the row of the second cell: "))
-c2 = int(input("Insert the column of the second cell: "))
-if r1 == r2 or c1 == c2:
-    print("Yes")
-else:
-    print("No")
